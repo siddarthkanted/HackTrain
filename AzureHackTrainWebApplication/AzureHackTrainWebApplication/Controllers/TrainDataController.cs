0776592c25db2cca519b66f0ba2b71c05864109e
siddarth <sikanted@microsoft.com>
2016-01-29 18:38:48 +0530

    webapi getTrainOnTrainNumber and getTrainListOnStationCode is created.

---
+++ AzureHackTrainWebApplication/AzureHackTrainWebApplication/Controllers/TrainDataController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Web.Http;
+using System.Xml.Serialization;
+using AzureHackTrainWebApplication.Models;
+
+namespace AzureHackTrainWebApplication.Controllers
+{
+    public class TrainDataController : ApiController
+    {
+
+        private Trains trains;
+
+        public TrainDataController()
+        {
+            trains = ConvertTrainsXmlToTrainsObject();
+        }
+
+        //http://localhost:64548/getTrainListOnStationCode?fromStationCode=be&toStationCode=mb
+        [Route("getTrainListOnStationCode")]
+        public List<Train> GetTrainListOnStationCode(string fromStationCode, string toStationCode)
+        {
+            //trains which are passing fromStation and ToStation
+            List<Train> trainList =
+                trains.Train.FindAll(
+                    x => x.TrainRecord.TrainScheduleItem.Exists(y => y.StationCode.Equals(fromStationCode)) &&
+                         x.TrainRecord.TrainScheduleItem.Exists(y => y.StationCode.Equals(toStationCode)));
+
+            //fromStation day should be less than  ToStation day
+            trainList =
+                trainList.FindAll(
+                    x =>
+                        (x.TrainRecord.TrainScheduleItem.Find(y => y.StationCode.Equals(fromStationCode)).Day <=
+                         x.TrainRecord.TrainScheduleItem.Find(y => y.StationCode.Equals(toStationCode)).Day));
+
+
+            //fromStation time should be less than  ToStation time
+            trainList =
+                trainList.FindAll(x =>
+                    (IsFromStationTimeLessThanToStationTime(
+                        x.TrainRecord.TrainScheduleItem.Find(y => y.StationCode.Equals(fromStationCode))
+                            .DepartureTime,
+                        x.TrainRecord.TrainScheduleItem.Find(y => y.StationCode.Equals(toStationCode))
+                            .DepartureTime))
+                    );
+
+            return trainList;
+        }
+
+        private bool IsFromStationTimeLessThanToStationTime(string fromStationDepatureTimeString, string toStationDepatureTimeString)
+        {
+            if (fromStationDepatureTimeString.Equals("Destination")) return false;
+            if (toStationDepatureTimeString.Equals("Destination")) return true;
+
+            TimeSpan fromStationDepatureTime = DateTime.Parse(fromStationDepatureTimeString).TimeOfDay;
+            TimeSpan toStationDepatureTime = DateTime.Parse(toStationDepatureTimeString).TimeOfDay;
+
+            return TimeSpan.Compare(fromStationDepatureTime, toStationDepatureTime) < 0;
+        }
+
+        //http://localhost:64548/getTrainOnTrainNumber?trainNumber=1234
+        [Route("getTrainOnTrainNumber")]
+        public Train GetTrainOnTrainNumber(string trainNumber)
+        {
+            Train train = trains.Train.Find(x => x.TrainDetails.TrainNumber.Equals(trainNumber));
+            return train;
+        }
+
+
+
+        private Trains ConvertTrainsXmlToTrainsObject()
+        {
+            XmlSerializer trainsXmlSerializer = new XmlSerializer(typeof(Trains));
+            TextReader trainsTextReader = new StreamReader(System.Web.HttpContext.Current.Server.MapPath(@"~/Data/TrainData.xml"));
+            Trains trains = (Trains)trainsXmlSerializer.Deserialize(trainsTextReader);
+            trainsTextReader.Close();
+            return trains;
+        }
+    }
+}
---
+++ AzureHackTrainWebApplication/AzureHackTrainWebApplication/Models/Train.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace AzureHackTrainWebApplication.Models
+{
+    [XmlRoot(ElementName = "TrainScheduleItem")]
+    public class TrainScheduleItem
+    {
+        [XmlElement(ElementName = "serialNumber")]
+        public string SerialNumber { get; set; }
+        [XmlElement(ElementName = "stationCode")]
+        public string StationCode { get; set; }
+        [XmlElement(ElementName = "stationName")]
+        public string StationName { get; set; }
+        [XmlElement(ElementName = "arrivalTime")]
+        public string ArrivalTime { get; set; }
+        [XmlElement(ElementName = "departureTime")]
+        public string DepartureTime { get; set; }
+        [XmlElement(ElementName = "day")]
+        public int Day { get; set; }
+        [XmlElement(ElementName = "halt")]
+        public string Halt { get; set; }
+        [XmlElement(ElementName = "distance")]
+        public string Distance { get; set; }
+        [XmlElement(ElementName = "latitude")]
+        public string Latitude { get; set; }
+        [XmlElement(ElementName = "longitude")]
+        public string Longitude { get; set; }
+        [XmlElement(ElementName = "isHalting")]
+        public string IsHalting { get; set; }
+    }
+
+    [XmlRoot(ElementName = "TrainRecord")]
+    public class TrainRecord
+    {
+        [XmlElement(ElementName = "Provider")]
+        public string Provider { get; set; }
+        [XmlElement(ElementName = "TrainName")]
+        public string TrainName { get; set; }
+        [XmlElement(ElementName = "TrainNumber")]
+        public string TrainNumber { get; set; }
+        [XmlElement(ElementName = "TrainScheduleItem")]
+        public List<TrainScheduleItem> TrainScheduleItem { get; set; }
+    }
+
+    [XmlRoot(ElementName = "DaysRunning")]
+    public class DaysRunning
+    {
+        [XmlElement(ElementName = "Day")]
+        public List<string> Day { get; set; }
+    }
+
+    [XmlRoot(ElementName = "TrainAttributes")]
+    public class TrainAttributes
+    {
+        [XmlElement(ElementName = "isPantryAvailable")]
+        public string IsPantryAvailable { get; set; }
+        [XmlElement(ElementName = "isCateringAvailable")]
+        public string IsCateringAvailable { get; set; }
+        [XmlElement(ElementName = "hasPrepaidTaxiService")]
+        public string HasPrepaidTaxiService { get; set; }
+        [XmlElement(ElementName = "hasPrepaidAutoService")]
+        public string HasPrepaidAutoService { get; set; }
+        [XmlElement(ElementName = "DaysRunning")]
+        public DaysRunning DaysRunning { get; set; }
+        [XmlElement(ElementName = "trainType")]
+        public string TrainType { get; set; }
+        [XmlElement(ElementName = "fareType")]
+        public string FareType { get; set; }
+        [XmlElement(ElementName = "pairTrainNumber")]
+        public string PairTrainNumber { get; set; }
+        [XmlElement(ElementName = "pairTrainName")]
+        public string PairTrainName { get; set; }
+        [XmlElement(ElementName = "ClassesAvailable")]
+        public ClassesAvailable ClassesAvailable { get; set; }
+        [XmlElement(ElementName = "RakeShareTrainNumbers")]
+        public RakeShareTrainNumbers RakeShareTrainNumbers { get; set; }
+        [XmlElement(ElementName = "CoachComposition")]
+        public CoachComposition CoachComposition { get; set; }
+    }
+
+    [XmlRoot(ElementName = "TrainDetails")]
+    public class TrainDetails
+    {
+        [XmlElement(ElementName = "Provider")]
+        public string Provider { get; set; }
+        [XmlElement(ElementName = "TrainName")]
+        public string TrainName { get; set; }
+        [XmlElement(ElementName = "TrainNumber")]
+        public string TrainNumber { get; set; }
+        [XmlElement(ElementName = "TrainAttributes")]
+        public TrainAttributes TrainAttributes { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Train")]
+    public class Train
+    {
+        [XmlElement(ElementName = "TrainRecord")]
+        public TrainRecord TrainRecord { get; set; }
+        [XmlElement(ElementName = "TrainDetails")]
+        public TrainDetails TrainDetails { get; set; }
+    }
+
+    [XmlRoot(ElementName = "ClassesAvailable")]
+    public class ClassesAvailable
+    {
+        [XmlElement(ElementName = "Class")]
+        public List<string> Class { get; set; }
+    }
+
+    [XmlRoot(ElementName = "RakeShareTrainNumbers")]
+    public class RakeShareTrainNumbers
+    {
+        [XmlElement(ElementName = "RakeShareTrainNumber")]
+        public List<string> RakeShareTrainNumber { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CoachAttributes")]
+    public class CoachAttributes
+    {
+        [XmlElement(ElementName = "CarNumber")]
+        public string CarNumber { get; set; }
+        [XmlElement(ElementName = "CarType")]
+        public string CarType { get; set; }
+        [XmlElement(ElementName = "CarName")]
+        public string CarName { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CoachComposition")]
+    public class CoachComposition
+    {
+        [XmlElement(ElementName = "CoachAttributes")]
+        public List<CoachAttributes> CoachAttributes { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Trains")]
+    public class Trains
+    {
+        [XmlElement(ElementName = "Train")]
+        public List<Train> Train { get; set; }
+    }
+}
