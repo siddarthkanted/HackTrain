8b4b835fd719cca593a833f4ce90997c92ad95da
siddarth <sikanted@microsoft.com>
2016-01-30 11:30:50 +0530

    TrainData Api controller methods returns json Added Train_hyd_sc_kcg xml file Method IsFromStationDayTimeLessThanToStationDayTime bug fixed.

--- AzureHackTrainWebApplication/AzureHackTrainWebApplication/Controllers/TrainDataController.cs
+++ AzureHackTrainWebApplication/AzureHackTrainWebApplication/Controllers/TrainDataController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Web.Http;
+using System.Web.Http.Results;
 using System.Xml.Serialization;
 using AzureHackTrainWebApplication.Models;
 
@@ -21,9 +22,9 @@ namespace AzureHackTrainWebApplication.Controllers
             trains = ConvertTrainsXmlToTrainsObject();
         }
 
-        //http://localhost:64548/getTrainListOnStationCode?fromStationCode=be&toStationCode=mb
+        //http://localhost:64548/getTrainListOnStationCode?fromStationCode=cbe&toStationCode=hyb
         [Route("getTrainListOnStationCode")]
-        public List<Train> GetTrainListOnStationCode(string fromStationCode, string toStationCode)
+        public JsonResult<List<Train>> GetTrainListOnStationCode(string fromStationCode, string toStationCode)
         {
             //trains which are passing fromStation and ToStation
             List<Train> trainList =
@@ -31,44 +32,35 @@ namespace AzureHackTrainWebApplication.Controllers
                     x => x.TrainRecord.TrainScheduleItem.Exists(y => y.StationCode.Equals(fromStationCode)) &&
                          x.TrainRecord.TrainScheduleItem.Exists(y => y.StationCode.Equals(toStationCode)));
 
-            //fromStation day should be less than  ToStation day
-            trainList =
-                trainList.FindAll(
-                    x =>
-                        (x.TrainRecord.TrainScheduleItem.Find(y => y.StationCode.Equals(fromStationCode)).Day <=
-                         x.TrainRecord.TrainScheduleItem.Find(y => y.StationCode.Equals(toStationCode)).Day));
-
-
-            //fromStation time should be less than  ToStation time
+            //fromStation DayTime should be less than  ToStation DayTime
             trainList =
                 trainList.FindAll(x =>
-                    (IsFromStationTimeLessThanToStationTime(
-                        x.TrainRecord.TrainScheduleItem.Find(y => y.StationCode.Equals(fromStationCode))
-                            .DepartureTime,
-                        x.TrainRecord.TrainScheduleItem.Find(y => y.StationCode.Equals(toStationCode))
-                            .DepartureTime))
-                    );
+                    (IsFromStationDayTimeLessThanToStationDayTime(
+                        x.TrainRecord.TrainScheduleItem.Find(y => y.StationCode.Equals(fromStationCode)),
+                        x.TrainRecord.TrainScheduleItem.Find(y => y.StationCode.Equals(toStationCode)))));
 
-            return trainList;
+            return Json(trainList);
         }
 
-        private bool IsFromStationTimeLessThanToStationTime(string fromStationDepatureTimeString, string toStationDepatureTimeString)
+        private bool IsFromStationDayTimeLessThanToStationDayTime(TrainScheduleItem fromStationTrainScheduleItem, TrainScheduleItem toStationTrainScheduleItem)
         {
-            if (fromStationDepatureTimeString.Equals("Destination")) return false;
-            if (toStationDepatureTimeString.Equals("Destination")) return true;
+            if (fromStationTrainScheduleItem.DepartureTime.Equals("Destination")) return false;
+            if (toStationTrainScheduleItem.DepartureTime.Equals("Destination")) return true;
+            if (fromStationTrainScheduleItem.Day > toStationTrainScheduleItem.Day) return false;
+            if (fromStationTrainScheduleItem.Day < toStationTrainScheduleItem.Day) return true;
 
-            TimeSpan fromStationDepatureTime = DateTime.Parse(fromStationDepatureTimeString).TimeOfDay;
-            TimeSpan toStationDepatureTime = DateTime.Parse(toStationDepatureTimeString).TimeOfDay;
+            TimeSpan fromStationDepatureTime = DateTime.Parse(fromStationTrainScheduleItem.DepartureTime).TimeOfDay;
+            TimeSpan toStationDepatureTime = DateTime.Parse(toStationTrainScheduleItem.DepartureTime).TimeOfDay;
 
             return TimeSpan.Compare(fromStationDepatureTime, toStationDepatureTime) < 0;
         }
 
-        //http://localhost:64548/getTrainOnTrainNumber?trainNumber=1234
+        //http://localhost:64548/getTrainOnTrainNumber?trainNumber=12647
         [Route("getTrainOnTrainNumber")]
-        public Train GetTrainOnTrainNumber(string trainNumber)
+        public JsonResult<Train> GetTrainOnTrainNumber(string trainNumber)
         {
             Train train = trains.Train.Find(x => x.TrainDetails.TrainNumber.Equals(trainNumber));
-            return train;
+            return Json(train);
         }
 
 
@@ -76,7 +68,7 @@ namespace AzureHackTrainWebApplication.Controllers
         private Trains ConvertTrainsXmlToTrainsObject()
         {
             XmlSerializer trainsXmlSerializer = new XmlSerializer(typeof(Trains));
-            TextReader trainsTextReader = new StreamReader(System.Web.HttpContext.Current.Server.MapPath(@"~/Data/TrainData.xml"));
+            TextReader trainsTextReader = new StreamReader(System.Web.HttpContext.Current.Server.MapPath(@"~/Data/Trains_Hyd_SC_KCG.xml"));
             Trains trains = (Trains)trainsXmlSerializer.Deserialize(trainsTextReader);
             trainsTextReader.Close();
             return trains;
